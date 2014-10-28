78c5260635f5929bd469ff6f0b8eeb48710acc7b
Jeremiah Johnson <jjohnson@insidesales.com>
2014-10-28 13:57:50 -0600

    add size

--- web/index.php
+++ web/index.php
@@ -14,7 +14,8 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  $size = strlen(file_get_contents('https://onedrive.live.com/embed?cid=FA17E0833EE4A1BF&resid=FA17E0833EE4A1BF%212406&authkey=AH0R1wo0FrQSf9Y&em=2&wdAllowInteractivity=False&Item=%27Sheet1%27!A1%3AC25&wdHideGridlines=True'));
+  return 'Hello ('.$size.')';
 });
 
 $app->run();
