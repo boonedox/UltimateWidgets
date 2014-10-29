e62164ac122cd0bec6820a7cf39372f2856f49b1
Jeremiah Johnson <jjohnson@insidesales.com>
2014-10-29 08:20:06 -0600

    more testing

--- web/index.php
+++ web/index.php
@@ -14,20 +14,22 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Our web handlers
 
 $app->get('/ultimate', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
+    $data = file_get_contents($url);
+    $dir = sys_get_temp_dir();
+    $inputFileName = $dir.'/tmp.xls';
+    file_put_contents($inputFileName, $data);
+    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+
+
+
+    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+    return "<pre>".print_r($sheetData, true);
+});
+$app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  $size = strlen(file_get_contents('https://onedrive.live.com/embed?cid=FA17E0833EE4A1BF&resid=FA17E0833EE4A1BF%212406&authkey=AH0R1wo0FrQSf9Y&em=2&wdAllowInteractivity=False&Item=%27Sheet1%27!A1%3AC25&wdHideGridlines=True'));
-$url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
-$data = file_get_contents($url);
-$dir = sys_get_temp_dir();
-$inputFileName = $dir.'/tmp.xls';
-file_put_contents($inputFileName, $data);
-$objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-
-
-
-$sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-return print_r($sheetData, true);
-  return 'Hello ('.$size.')';
+  return 'Hello';
 });
 
 $app->run();
