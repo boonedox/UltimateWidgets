6eb12b3e1133d8e12e34817cd54bdd6b0d8b13fe
Jeremiah Johnson <jjohnson@insidesales.com>
2014-10-29 22:49:48 -0600

    more testing

--- web/index.php
+++ web/index.php
@@ -1,3 +1,47 @@
+<?php
+exit;
+require('../vendor/autoload.php');
+function getData() {
+    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
+    $url = '../attendingDisc.xlsx';
+    $data = file_get_contents($url);
+    $dir = sys_get_temp_dir();
+    $inputFileName = $dir.'/tmp.xls';
+    file_put_contents($inputFileName, $data);
+    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+
+    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+    $stats['accepted'] = $sheetData[2]['B'];
+    $stats['last_update'] = date('Y-m-d H:i:s', strtotime($sheetData[2]['C']));
+    $stats['maybe'] = $sheetData[3]['B'];
+    $stats['declined'] = $sheetData[4]['B'];
+    $stats['pending'] = $sheetData[5]['B'];
+    for ($x = 8; $x <= count($sheetData); $x++) {
+      if (!empty($sheetData[$x]['A'])) {
+        $stats['people']['accepted'][] = $sheetData[$x]['A'];
+      }
+      if (!empty($sheetData[$x]['B'])) {
+        $stats['people']['maybe'][] = $sheetData[$x]['B'];
+      }
+      if (!empty($sheetData[$x]['C'])) {
+        $stats['people']['declined'][] = $sheetData[$x]['C'];
+      }
+    }
+    return json_encode($stats);
+}
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Our web handlers
+
+$app->get('/ultimate', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    $html =<<<HTML
 <html>
   <head>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
@@ -39,99 +83,25 @@
           chart.draw(data, options);
         }, 26000);
       }
+      $(document).ready(function() {
+        $.get('ultimate_data', function(data) {
+          alert(data);
+        },
+        'json');
+        
+
+      });
     </script>
   </head>
   <body>
     <div id="chart_div" style="width: 400px; height: 120px;"></div>
   </body>
 </html>
-<?php
-exit;
-require('../vendor/autoload.php');
-  //$size = strlen(file_get_contents('https://onedrive.live.com/embed?cid=FA17E0833EE4A1BF&resid=FA17E0833EE4A1BF%212406&authkey=AH0R1wo0FrQSf9Y&em=2&wdAllowInteractivity=False&Item=%27Sheet1%27!A1%3AC25&wdHideGridlines=True'));
-    $url = '../attendingDisc.xlsx';
-    $data = file_get_contents($url);
-    $dir = sys_get_temp_dir();
-    $inputFileName = $dir.'/tmp.xls';
-    file_put_contents($inputFileName, $data);
-    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-
-
-
-    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-    $stats['accepted'] = $sheetData[2]['B'];
-    $stats['last_update'] = date('Y-m-d H:i:s', strtotime($sheetData[2]['C']));
-    $stats['maybe'] = $sheetData[3]['B'];
-    $stats['declined'] = $sheetData[4]['B'];
-    $stats['pending'] = $sheetData[5]['B'];
-    for ($x = 8; $x <= count($sheetData); $x++) {
-      if (!empty($sheetData[$x]['A'])) {
-        $stats['people']['accepted'][] = $sheetData[$x]['A'];
-      }
-      if (!empty($sheetData[$x]['B'])) {
-        $stats['people']['maybe'][] = $sheetData[$x]['B'];
-      }
-      if (!empty($sheetData[$x]['C'])) {
-        $stats['people']['declined'][] = $sheetData[$x]['C'];
-      }
-    }
-    "<pre>".print_r($stats);
-    exit;
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/ultimate', function() use($app) {
-    $app['monolog']->addDebug('logging output.');
-    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
-    $url = '../attendingDisc.xlsx';
-    $data = file_get_contents($url);
-    $dir = sys_get_temp_dir();
-    $inputFileName = $dir.'/tmp.xls';
-    file_put_contents($inputFileName, $data);
-    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-
-    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-    $stats['accepted'] = $sheetData[2]['B'];
-    $stats['last_update'] = date('Y-m-d H:i:s', strtotime($sheetData[2]['C']));
-    $stats['maybe'] = $sheetData[3]['B'];
-    $stats['declined'] = $sheetData[4]['B'];
-    $stats['pending'] = $sheetData[5]['B'];
-    for ($x = 8; $x <= count($sheetData); $x++) {
-      if (!empty($sheetData[$x]['A'])) {
-        $stats['people']['accepted'][] = $sheetData[$x]['A'];
-      }
-      if (!empty($sheetData[$x]['B'])) {
-        $stats['people']['maybe'][] = $sheetData[$x]['B'];
-      }
-      if (!empty($sheetData[$x]['C'])) {
-        $stats['people']['declined'][] = $sheetData[$x]['C'];
-      }
-    }
-    return json_encode($stats);
+HTML;
 });
 $app->get('/ultimate_data', function() use($app) {
-    $app['monolog']->addDebug('logging output.');
-    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
-    $url = '../attendingDisc.xlsx';
-    $data = file_get_contents($url);
-    $dir = sys_get_temp_dir();
-    $inputFileName = $dir.'/tmp.xls';
-    file_put_contents($inputFileName, $data);
-    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-
-
-
-    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-    return "<pre>".print_r($sheetData, true);
-});
+    return getData();
+  });
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return 'Hello';
