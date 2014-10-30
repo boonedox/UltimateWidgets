ce7e707205e25c25354b1c807135bc36e694b928
Jeremiah Johnson <jjohnson@insidesales.com>
2014-10-30 00:07:37 -0600

    more testing

--- web/index.php
+++ web/index.php
@@ -4,8 +4,10 @@ require('../vendor/autoload.php');
 
 function getData()
 {
-    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
-    $url = '../attendingDisc.xlsx';
+    //$url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
+            //https://sc7acq-ch3301.files.1drv.com/y2mFMsOXDSaN3iVvvT7dA52quEuUtis-IWZv_bx9fu_D33b4nknKSYS1sOLEINoYAbiVsv6IMiTBR4xiBNj_9k-rk5o5XV_LWD12Nuo9xi-DoV4pU7KUQkXzRPIrj35dZLg/attendingDisc.xlsx?download&psid=1
+    $url = 'https://onedrive.live.com/download.aspx?cid=fa17e0833ee4a1bf&id=documents&resid=FA17E0833EE4A1BF%212406&authkey=!AH0R1wo0FrQSf9Y';
+    //$url = '../attendingDisc.xlsx';
     $data = file_get_contents($url);
     $dir = sys_get_temp_dir();
     $inputFileName = $dir.'/tmp.xls';
