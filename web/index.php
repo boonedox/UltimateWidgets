d763f0bad647db63bfad2ed610c2695315fbf91a
Jeremiah Johnson <jjohnson@insidesales.com>
2014-10-29 23:05:18 -0600

    more testing

--- web/index.php
+++ web/index.php
@@ -1,9 +1,34 @@
 <?php
-exit;
 require('../vendor/autoload.php');
 
 function getData()
 {
+    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
+    $url = '../attendingDisc.xlsx';
+    $data = file_get_contents($url);
+    $dir = sys_get_temp_dir();
+    $inputFileName = $dir.'/tmp.xls';
+    file_put_contents($inputFileName, $data);
+    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+
+    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
+    $stats['accepted'] = $sheetData[2]['B'];
+    $stats['last_update'] = date('Y-m-d H:i:s', strtotime($sheetData[2]['C']));
+    $stats['maybe'] = $sheetData[3]['B'];
+    $stats['declined'] = $sheetData[4]['B'];
+    $stats['pending'] = $sheetData[5]['B'];
+    for ($x = 8; $x <= count($sheetData); $x++) {
+        if (!empty($sheetData[$x]['A'])) {
+            $stats['people']['accepted'][] = $sheetData[$x]['A'];
+        }
+        if (!empty($sheetData[$x]['B'])) {
+            $stats['people']['maybe'][] = $sheetData[$x]['B'];
+        }
+        if (!empty($sheetData[$x]['C'])) {
+            $stats['people']['declined'][] = $sheetData[$x]['C'];
+        }
+    }
+    return json_encode($stats);
 }
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -65,32 +90,6 @@ HTML;
     return $html;
 });
 $app->get('/ultimate_data', function () use ($app) {
-    $url = 'https://sc7acq-ch3301.files.1drv.com/y2mFNc5dptk-Z6-mZx2NPkDn5Z4BnjtcxOmSg2yAkMkD-IdOzfdZuPem0tz2NEBYZGNnbfIaXkRb7a6E9RKgtx5ZlhnIZJfrYzGsrWrR4ImFt-XAoqAG6DInCnSYgO3irpv/attendingDisc.xlsx?download&psid=1';
-    $url = '../attendingDisc.xlsx';
-    $data = file_get_contents($url);
-    $dir = sys_get_temp_dir();
-    $inputFileName = $dir.'/tmp.xls';
-    file_put_contents($inputFileName, $data);
-    $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-
-    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
-    $stats['accepted'] = $sheetData[2]['B'];
-    $stats['last_update'] = date('Y-m-d H:i:s', strtotime($sheetData[2]['C']));
-    $stats['maybe'] = $sheetData[3]['B'];
-    $stats['declined'] = $sheetData[4]['B'];
-    $stats['pending'] = $sheetData[5]['B'];
-    for ($x = 8; $x <= count($sheetData); $x++) {
-        if (!empty($sheetData[$x]['A'])) {
-            $stats['people']['accepted'][] = $sheetData[$x]['A'];
-        }
-        if (!empty($sheetData[$x]['B'])) {
-            $stats['people']['maybe'][] = $sheetData[$x]['B'];
-        }
-        if (!empty($sheetData[$x]['C'])) {
-            $stats['people']['declined'][] = $sheetData[$x]['C'];
-        }
-    }
-    return json_encode($stats);
     return getData();
 });
 $app->get('/', function () use ($app) {
