cd38202ac87c4ee10ebfccfd7d27cdc7d9e77e5f
Jeremiah Johnson <jjohnson@insidesales.com>
2014-11-12 23:11:07 -0700

    add env var for weather api key, removed unused files

--- lib/Weather.php
+++ lib/Weather.php
@@ -4,20 +4,28 @@ namespace uw;
 
 class Weather
 {
+    private $api_key;
+
+    public function __construct()
+    {
+        $this->api_key = getenv('WEATHER_API_KEY');
+    }
+
     public function getHourlyWeatherForZip($zip)
     {
         $filename = sys_get_temp_dir().'/weather'.$zip;
         if (!file_exists($filename) || time() - filemtime($filename) > 600) {
-            $url = "http://api.wunderground.com/api/42efd44561264d34/hourly/q/{$zip}.json";
+            $url = "http://api.wunderground.com/api/{$this->api_key}/hourly/q/{$zip}.json";
             file_put_contents($filename, file_get_contents($url));
         }
         return file_get_contents($filename);
     }
+    
     public function getRecordWeatherForZip($zip)
     {
         $filename = sys_get_temp_dir().'/almanac'.$zip;
         if (!file_exists($filename) || time() - filemtime($filename) > 600) {
-            $url = "http://api.wunderground.com/api/42efd44561264d34/almanac/q/{$zip}.json";
+            $url = "http://api.wunderground.com/api/{$this->api_key}/almanac/q/{$zip}.json";
             file_put_contents($filename, file_get_contents($url));
         }
         return file_get_contents($filename);
