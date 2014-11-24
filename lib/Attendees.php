0d4cd2f1f473b269c6d7f6fbeba57d593806d2a5
Jeremiah Johnson <jjohnson@insidesales.com>
2014-11-24 13:17:43 -0700

    remove debug :(

--- lib/Attendees.php
+++ lib/Attendees.php
@@ -23,7 +23,7 @@ class Attendees
         //$this->logger->addDebug('retrieved file, last update: '.$sheetData[2]['C']);
         $ts = strtotime($sheetData[2]['C']);
 
-        if (false&& empty($_GET['debug']) && $ts < strtotime(date('Y-m-d'))) {
+        if (empty($_GET['debug']) && $ts < strtotime(date('Y-m-d'))) {
             //$this->logger->addDebug('last update is more than a day old, returning empty data');
             return array(
                 'accepted' => 0,
